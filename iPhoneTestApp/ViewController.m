c16e69ed9c12654534687549d24b921e85a65632
Fabrizio Bertoglio <fabrizio.bertoglio@gmail.com>
2023-06-17 17:36:28 +0800

    reproducing issue in objc

--- iPhoneTestApp/ViewController.m
+++ iPhoneTestApp/ViewController.m
@@ -7,14 +7,18 @@
 
 #import "ViewController.h"
 
-@interface RCTUITextField : UITextField
+@interface RCTUITextView : UITextView<UITextViewDelegate>
 @end
 
-@implementation RCTUITextField {}
+@implementation RCTUITextView {}
 
-- (CGRect)textRectForBounds:(CGRect)bounds
+-(void)textViewDidChange:(UITextView *)textView
 {
-  return CGRectMake(0, -50, 200, 100);
+  NSLog(@"textViewDidChange : %@",textView.text);
+  NSMutableDictionary<NSAttributedStringKey, id> *attributes = [NSMutableDictionary dictionaryWithCapacity:10];
+  attributes[NSForegroundColorAttributeName] = [UIColor blueColor];
+  NSAttributedString *attributedString = [[NSAttributedString alloc] initWithString:textView.text attributes: attributes];
+  [textView setAttributedText: attributedString];
 }
 
 @end
@@ -28,14 +32,12 @@
 - (void)viewDidLoad {
   [super viewDidLoad];
   // Do any additional setup after loading the view.
-  UITextView *textView = [[UITextView alloc] initWithFrame:CGRectMake(10, 200, 300, 150)];
+  RCTUITextView *textView = [[RCTUITextView alloc] initWithFrame:CGRectMake(10, 200, 300, 50)];
+  textView.scrollEnabled = true;
+  textView.delegate = textView;
   UIView *redBackgroundView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height / 2.0)];
   redBackgroundView.backgroundColor = [UIColor redColor];
   [self.view addSubview:redBackgroundView];
-  NSMutableDictionary<NSAttributedStringKey, id> *attributes = [NSMutableDictionary dictionaryWithCapacity:10];
-  attributes[NSForegroundColorAttributeName] = [UIColor blueColor];
-  NSAttributedString *attributedString = [[NSAttributedString alloc] initWithString:@"my test attributed string" attributes: attributes];
-  textView.attributedText = attributedString;
   [redBackgroundView addSubview:textView];
 }
 
