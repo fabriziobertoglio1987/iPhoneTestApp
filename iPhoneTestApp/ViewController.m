b211e02b5da44798299989da0ed38d390547c21f
Fabrizio Bertoglio <fabrizio.bertoglio@gmail.com>
2023-06-26 21:22:55 +0800

    adding example

--- iPhoneTestApp/ViewController.m
+++ iPhoneTestApp/ViewController.m
@@ -10,13 +10,31 @@
 @interface RCTUITextView : UITextView<UITextViewDelegate>
 @end
 
-@implementation RCTUITextView {}
+@implementation RCTUITextView {
+  NSAttributedString *_oldAttributedString;
+  NSMutableDictionary<NSAttributedStringKey, id> *_attributes;
+}
 
 -(void)textViewDidChange:(UITextView *)textView
 {
-  NSLog(@"textViewDidChange : %@",textView.text);
-  NSString *newText = [NSString stringWithFormat:@"%@/%@/%@", textView.text, @"e"];
-  textView.text = newText;
+  if ([textView.text isEqualToString:@"1\n\n2\n3\n"]) {
+    NSMutableAttributedString *attributedStringCopy = [textView.attributedText mutableCopy];
+    [textView.attributedText enumerateAttribute:NSParagraphStyleAttributeName inRange:NSMakeRange(0, [textView.attributedText length]) options:0 usingBlock:^(id  _Nullable value, NSRange range, BOOL * _Nonnull stop) {
+      if ([value isKindOfClass:[NSParagraphStyle class]]) {
+        NSMutableParagraphStyle *style = [value mutableCopy];
+        // enable one of this changes to the attributes to reproduce the issue
+        // style.minimumLineHeight = 20;
+        // [attributedStringCopy addAttribute:NSForegroundColorAttributeName value:[UIColor greenColor] range:range];
+        [attributedStringCopy addAttribute:NSParagraphStyleAttributeName value:style range:range];
+      }
+    }];
+    [textView setAttributedText: attributedStringCopy];
+  } else {
+    NSMutableDictionary<NSAttributedStringKey, id> *attributes = [NSMutableDictionary dictionaryWithCapacity:10];
+    attributes[NSForegroundColorAttributeName] = [UIColor redColor];
+    NSAttributedString *attributedString = [[NSAttributedString alloc] initWithString:textView.text attributes: attributes];
+    [textView setAttributedText:attributedString];
+  }
 }
 
 @end
@@ -32,7 +50,9 @@
   // Do any additional setup after loading the view.
   RCTUITextView *textView = [[RCTUITextView alloc] initWithFrame:CGRectMake(10, 200, 300, 50)];
   textView.scrollEnabled = true;
+
   textView.delegate = textView;
+
   UIView *redBackgroundView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height / 2.0)];
   redBackgroundView.backgroundColor = [UIColor redColor];
   [self.view addSubview:redBackgroundView];
